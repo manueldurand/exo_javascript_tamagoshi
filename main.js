7e9270dc1064f23cab2a04847449f5b8c54dd9f6
manuel_durand <manuel.durand@mailo.com>
2022-11-24 01:06:05 +0100

    correction

--- main.js
+++ main.js
@@ -1,27 +1,21 @@
 function quest_genre(){
     let rep_genre = prompt("Quel est le genre de votre Tamagoshi ?", "réponse ici");
     rep_genre = rep_genre.toLowerCase();
-    if (rep_genre == "femelle" || rep_genre == "male" || rep_genre == "mâle"){
-        alert("merci pour votre réponse. C'est noté.")
-    }
-    else {
-        alert("Je n'ai pas compris votre réponse.")
+    while (rep_genre != "femelle" && rep_genre != "male" && rep_genre != "mâle"){
+        rep_genre = prompt("Quel est le genre de votre Tamagoshi ?", "réponse ici");
     }
 }
-// alternative avec switch, possibilité de varier les messages...
-// 
-//     switch (rep_genre){
-//         case "femelle":
-//             alert("merci pour votre réponse.");
-//             break;
-//         case "mâle":
-//             alert("merci pour votre réponse.");
-//             break;
-//         case "male":
-//             alert("merci pour votre réponse.");
-//             break;
-//         default:
-//             alert("je n'ai pas compris votre réponse");
-//             quest_genre();
-//     }
+
+// correction : avec replace()
+
+// let reponse = prompt("quel sexe pour votre Tama ? ").toLowerCase.replace("â", "a");
+// while (reponse != "male" && reponse != "femelle"){
+//     reponse = prompt("quel sexe pour votre Tama ? ").toLowerCase.replace("â", "a");
 // }
+
+// avec fonction : 
+// function traitement(texte){
+//     return texte.toLowerCase().replace("â","a");
+// } puis 
+
+// let reponse = traitement(prompt("quel est le sexe souhaité ?"));
